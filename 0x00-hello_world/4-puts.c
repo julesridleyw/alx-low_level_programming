fa8b7fdb239be63625dbd90751189310dc019def
julesridleyw <juleswasonga009@gmail.com>
2023-06-15 07:11:13 -0700

    puts updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  *main- Serves as the entry point for the program
  *
- *Return: By default returns zero if no errors otherwise returns non-zero value.
+ *Return: By default returns zero if no error otherwise returns non-zero value.
  */
 int main(void)
 {
